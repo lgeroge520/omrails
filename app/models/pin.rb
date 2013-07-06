b9294e979aedadd187cc74c8745760197dfee026
FANXIFAN <lgeroge@hotmail.com>
2013-07-06 11:37:31 +0700

    AddUserToPin

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
   attr_accessible :description
 
   validates :description, presence: true # length: { maximum: 50}
+
+  belongs_to :user
+  validates :user_id, presence: true
+
 end
